edbdb2b26f07aebc10ff1884244d9b31df846993
Sk8Mahammad-Irfan9 <skirfan0890@gmail.com>
2025-02-16 19:31:37 +0530

    workflow router

--- controller/workflow.controller.js
+++ controller/workflow.controller.js
@@ -23,6 +23,15 @@ export const sendReminders = serve(async (context) => {
   }
   for (const daysBefore of REMINDERS) {
     const reminderDate = renewalDate.subtract(daysBefore, "day");
+
+    if (reminderDate.isAfter(dayjs())) {
+      await sleepUntillReminder(
+        context,
+        `Reminder ${daysBefore} days before.`,
+        reminderDate
+      );
+    }
+    await triggerReminder(context,`Reminder ${daysBefore} days before`)
   }
 });
 
