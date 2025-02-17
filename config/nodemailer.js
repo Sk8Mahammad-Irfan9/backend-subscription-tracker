618a43901801efcbe9d3ee6018137b99fa342822
Sk8Mahammad-Irfan9 <skirfan0890@gmail.com>
2025-02-17 10:48:19 +0530

    email changes done

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,10 +1,12 @@
 import nodemailer from "nodemailer";
 import { EMAIL_PASSWORD } from "./env.js";
 
+export const accountEmail = "skirfan0890@gmail.com";
+
 const transport = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "skirfan0890@gmail.com",
+    user: accountEmail,
     pass: EMAIL_PASSWORD,
   },
 });
